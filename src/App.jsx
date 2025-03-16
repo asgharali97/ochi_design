adaf2348abd7d816e403262e73ff7bedace13e0c
asgharali97 <asgharalifs48@gmail.com>
2025-03-15 22:02:19 -0700

    implement NavBar component and custom fonts.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
+import react from 'react'
+import LocomotiveScroll from 'locomotive-scroll';
+import NavBar from './Components/NavBar';
+
 
 function App() {
+  const locomotiveScroll = new LocomotiveScroll();
 
   return (
     <>
-     
+     <div className="w-full h-screen bg-zinc-900 text-white">
+      <NavBar />
+     </div>
     </>
   )
 }
